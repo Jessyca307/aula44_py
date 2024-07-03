aa36271934a2a64396a36149f987aa27a8431f0b
Jessyca307 <131052960+Jessyca307@users.noreply.github.com>
2024-07-02 21:46:50 -0300

    Sobre map

---
+++ aula44.py
@@ -0,0 +1,41 @@
+# map - para mapear dados
+from functools import partial
+from types import GeneratorType
+
+def print_iter(iterator):
+          print(*list(iterator), sep='\n')
+          print()
+
+produtos = [
+        {'nome': 'produto 5', 'preco': 10.00},
+        {'nome': 'produto 1', 'preco': 22.32},
+        {'nome': 'produto 3', 'preco': 10.11},
+        {'nome': 'produto 2', 'preco': 105.87},
+        {'nome': 'produto 4', 'preco': 69.90},
+]
+
+def aumentar_porcentagem(valor, porcentagem):
+        return round(valor * porcentagem, 2)
+
+
+aumentar_dez_porcento = partial(aumentar_porcentagem,
+                                porcentagem= 1.1)
+
+
+def muda_preco_de_produtos(produto):
+        return {**produto, 'preco': aumentar_dez_porcento(produto['preco'])}
+
+
+novos_produtos = list(map(
+        muda_preco_de_produtos, produtos
+))
+
+
+print_iter(produtos)
+print_iter(novos_produtos)
+print(*novos_produtos, sep='\n' )
+
+#print(novos_produtos)
+print(hasattr(novos_produtos, '__iter__'))
+print(hasattr(novos_produtos, '__next__'))
+print(isinstance(novos_produtos, GeneratorType))
